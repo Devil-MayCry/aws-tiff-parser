1901d1af247109f375daffd2e4872c9d91b8c0e9
huteng <huteng@gagogroup.com>
2017-05-25 20:31:35 +0800

    fix(docker): docker file issue

--- bin/build.sh
+++ bin/build.sh
@@ -3,6 +3,8 @@
 BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="$(dirname "$BIN_DIR")"
 
+git pull origin master
+
 # update dependencies
 npm install --registry=http://npmjs.gagogroup.cn
 
--- bin/start_from_docker.sh
+++ bin/start_from_docker.sh
@@ -1,8 +1,5 @@
 echo "start delpoying..."
 
-# use production as env
-export NODE_ENV=production
-
 # delpoy by pm2-docker
 cd /usr/local/gdal-tiff-sentinel-aws/
 pm2-docker start "dist/app.js" -i 0
