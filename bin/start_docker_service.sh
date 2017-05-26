dea7d7ca65b9f25f3482acdb4a5ae53f21c97415
huteng <huteng@gagogroup.com>
2017-05-26 09:00:44 +0800

    fix(docker): change docker mount dir solution

--- bin/start_docker_service.sh
+++ bin/start_docker_service.sh
@@ -1,3 +1,3 @@
 # start docker and run the service bash script to run the service
 
-docker run -dit docker.gagogroup.cn:5000/gdal-tiff-sentinel-aws:latest /usr/local/gdal-tiff-sentinel-aws/bin/start_from_docker.sh
+docker run -dit -v /home/ec2-user/s3-gagobucket/:/home/ec2-user/s3-gagobucket/ -v /home/ec2-user/s3-sentinel-2/tiles/:/home/ec2-user/s3-sentinel-2/tiles/ docker.gagogroup.cn:5000/gdal-tiff-sentinel-aws:latest /usr/local/gdal-tiff-sentinel-aws/bin/start_from_docker.sh
