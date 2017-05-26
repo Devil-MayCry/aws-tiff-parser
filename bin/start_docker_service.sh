e19f639897baab7226062cbf6bdd39d71b3b9ed9
huteng <huteng@gagogroup.com>
2017-05-26 15:22:33 +0800

    feat(docker): change the the way deploy in docker

--- bin/start_docker_service.sh
+++ bin/start_docker_service.sh
@@ -1,3 +1,4 @@
 # start docker and run the service bash script to run the service
+chmod 777 /home/huteng/www/gdal-tiff-sentinel-aws/bin/start_from_docker.sh
 
-docker run -dit -v /home/ec2-user/s3-gagobucket/:/home/ec2-user/s3-gagobucket/ -v /home/ec2-user/s3-sentinel-2/tiles/:/home/ec2-user/s3-sentinel-2/tiles/ docker.gagogroup.cn:5000/gdal-tiff-sentinel-aws:latest /usr/local/gdal-tiff-sentinel-aws/bin/start_from_docker.sh
+docker run -it -v /home/huteng/www/gdal-tiff-sentinel-aws/:/usr/local/gdal-tiff-sentinel-aws/ -v /home/ec2-user/s3-gagobucket/:/home/ec2-user/s3-gagobucket/ -v /home/ec2-user/s3-sentinel-2/tiles/:/home/ec2-user/s3-sentinel-2/tiles/ docker.gagogroup.cn:5000/gdal:latest sh /usr/local/gdal-tiff-sentinel-aws/bin/start_from_docker.sh
--- bin/start_from_docker.sh
+++ bin/start_from_docker.sh
@@ -2,6 +2,9 @@ echo "start delpoying..."
 
 # delpoy by pm2-docker
 cd /usr/local/gdal-tiff-sentinel-aws/
+
+npm install --registry=http://npmjs.gagogroup.cn
+
 pm2-docker start "dist/app.js" -i 0
 
 echo "end delpoying"
