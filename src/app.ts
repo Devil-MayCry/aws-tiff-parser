7415a4cb958e17fa9fc0af0a4060c2cc1a57648c
huteng <huteng@gagogroup.com>
2017-06-01 22:51:05 +0800

    feat(tiff): start to produce B03-B08

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
 import {TiffTilerService} from "./tifftiler/tifftilerservice";
 
-TiffTilerService.startTransformImageToTiff(2017, 5, 8, ["B01", "B02"]);
+TiffTilerService.startTransformImageToTiff(2017, 5, 8, ["B03", "B04", "B05", "B06", "B07", "B08"]);
 console.log("Server start");
