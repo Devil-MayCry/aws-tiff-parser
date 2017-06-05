9a1bcf8bf0ec88e9dc27409dccd0bb485593fd05
huteng <huteng@gagogroup.com>
2017-06-05 11:22:32 +0800

    fix(bin): change redis start solution

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import {TiffTilerService} from "./tifftiler/tifftilerservice";
 
 
 
-// TiffTilerService.saveImagePathInRedis(2017, 5, 16 , ["B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08"]);
+TiffTilerService.saveImagePathInRedis(2017, 5, 16 , ["B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08"]);
 
-TiffTilerService.startTransformImageToTiff(8);
+// TiffTilerService.startTransformImageToTiff(8);
 console.log("Server start");
